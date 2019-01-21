1ab221259616bd7190a4716f44d2e3493f2a8ee0
dimchel <d.kidakov@space307.com>
2019-01-21 14:57:34 +0300

    fixed item update logic

--- app/src/main/java/com/dimchel/revolut/features/converter/presentation/ConverterAdapter.kt
+++ app/src/main/java/com/dimchel/revolut/features/converter/presentation/ConverterAdapter.kt
@@ -1,14 +1,17 @@
 package com.dimchel.revolut.features.converter.presentation
 
 import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import com.dimchel.revolut.R
 import com.dimchel.revolut.features.converter.models.RateModel
 import java.math.BigDecimal
 import java.util.*
+
+
 
 
 interface ConverterListListener {
@@ -28,7 +31,7 @@ class ConverterAdapter(
     private var ratesList: MutableList<RateModel> = arrayListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): ConverterViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_rate, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(com.dimchel.revolut.R.layout.item_rate, parent, false)
 
         return ConverterViewHolder(view)
     }
@@ -36,35 +39,7 @@ class ConverterAdapter(
     override fun getItemCount(): Int = ratesList.size
 
     override fun onBindViewHolder(holder: ConverterViewHolder, position: Int) {
-        val currentRateModel: RateModel = ratesList[position]
-
-        holder.currencyNameTextView.text = currentRateModel.name
-        holder.currencyValueEditText.text = multiplyValues(currentRateModel.value, inputValue)
-
-        if (position == SELECTED_ITEM_POSITION) {
-//            holder.currencyValueEditText.addTextChangedListener(object : TextWatcher {
-//                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
-//                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
-//
-//                override fun afterTextChanged(text: Editable?) {
-//                    Log.v("123", "text: " + text.toString() + "input: " + inputValue + " position: " + holder.adapterPosition)
-//                }
-//            })
-        } else {
-            holder.currencyValueEditText.setOnFocusChangeListener { _, _ ->
-                if (holder.adapterPosition != SELECTED_ITEM_POSITION) {
-                    listener.onItemSelected(currentRateModel)
-                    listener.onInputValueChanged(holder.currencyValueEditText.text.toString().toDouble())
-                }
-            }
-        }
-
-        holder.itemView.setOnClickListener {
-            if (position != SELECTED_ITEM_POSITION) {
-                listener.onItemSelected(ratesList[position])
-                listener.onInputValueChanged(holder.currencyValueEditText.text.toString().toDouble())
-            }
-        }
+        holder.bind(ratesList[position], inputValue, listener)
     }
 
     fun updateRates(newRatesList: List<RateModel>) {
@@ -100,15 +75,61 @@ class ConverterAdapter(
 
         notifyItemRangeChanged(0, itemCount - 1)
     }
-
-    private fun multiplyValues(first: Double, second: Double): String {
-        return BigDecimal(first.toString()).multiply(BigDecimal(second.toString())).stripTrailingZeros().toPlainString()
-    }
 }
 
 class ConverterViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
-    val currencyNameTextView: TextView = itemView.findViewById(R.id.item_rate_currency_textview)
-    val currencyValueEditText: TextView = itemView.findViewById(R.id.item_rate_value_edittext)
+    private val currencyNameTextView: TextView = itemView.findViewById(com.dimchel.revolut.R.id.item_rate_currency_textview)
+    private val currencyValueEditText: TextView = itemView.findViewById(com.dimchel.revolut.R.id.item_rate_value_edittext)
+
+    private var inputValue: Double? = null
+    private var listener: ConverterListListener? = null
+
+    private var selectionPosition: Int? = null
+
+    private val textWatcher: TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
+        override fun afterTextChanged(text: Editable?) {
+            if (adapterPosition == SELECTED_ITEM_POSITION) {
+                if (inputValue != text.toString().toDouble()) {
+                    listener!!.onInputValueChanged(text.toString().toDouble())
+                }
+            }
+        }
+    }
+
+    fun bind(currentRateModel: RateModel, inputValue: Double, listener: ConverterListListener) {
+        this.inputValue = inputValue
+        this.listener = listener
+
+        currencyValueEditText.removeTextChangedListener(textWatcher)
+
+        currencyNameTextView.text = currentRateModel.name
+        currencyValueEditText.text = multiplyValues(currentRateModel.value, inputValue)
+
+        if (adapterPosition == SELECTED_ITEM_POSITION) {
+            currencyValueEditText.addTextChangedListener(textWatcher)
+
+        } else {
+            currencyValueEditText.setOnFocusChangeListener { _, _ ->
+                if (adapterPosition != SELECTED_ITEM_POSITION) {
+                    listener.onItemSelected(currentRateModel)
+                    listener.onInputValueChanged(currencyValueEditText.text.toString().toDouble())
+                }
+            }
+        }
+
+        itemView.setOnClickListener {
+            if (adapterPosition != SELECTED_ITEM_POSITION) {
+                listener.onItemSelected(currentRateModel)
+                listener.onInputValueChanged(currencyValueEditText.text.toString().toDouble())
+            }
+        }
+    }
+
+    private fun multiplyValues(first: Double, second: Double): String =
+        BigDecimal(first.toString()).multiply(BigDecimal(second.toString())).stripTrailingZeros().toPlainString()
 
 }
